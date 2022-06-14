43cd6d5debedfe6accb44d11e45a8d8bc5c2adac
mahabir <mahabir7599@gmail.com>
2022-06-14 22:08:35 +0530

    HIS-301 variable added

--- src/main/java/com/example/demo/EurekaClient4Application.java
+++ src/main/java/com/example/demo/EurekaClient4Application.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class EurekaClient4Application {
 
 	public static void main(String[] args) {
+		int y = 200;
 		SpringApplication.run(EurekaClient4Application.class, args);
 	}
 
